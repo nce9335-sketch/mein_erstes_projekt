84d4f396112dc7392d7feae10e3145f9d6647b86
nce9335-sketch <nce9335@gmail.com>
2026-04-28 00:06:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "wichtig"
+print("hallo, ich schreibe dir eine kurze Nachricht. Dieses Projekt ist sehr "
+      + name + " " + name + " " + name + " " + name + " für mich. ich küsse dich.")
+firmenname ="nurcekiCGMBH"
+berufsfeld = "Data Analyst"
+weiterbildungsdauer = 4 +2
+print("Hallo liebes Team von " + firmenname + " ich möchte gerne bei euch dem Firma " + firmenname + " arbeiten.ich würde mich über eine Antwort freuen!")
+print("ich habe eine weiterbildung zum " + berufsfeld + " gemacht")
+print(weiterbildungsdauer)
