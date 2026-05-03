6b0b356e26c2bd2c010a74209e32092d016dfb7f
nce9335-sketch <nce9335@gmail.com>
2026-05-03 16:14:47 +0200

    Add files via upload
    Variablen
    Einfache Datentypen
    integer & float

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import nturl2path
+
 name = "wichtig"
 print("hallo, ich schreibe dir eine kurze Nachricht. Dieses Projekt ist sehr "
       + name + " " + name + " " + name + " " + name + " für mich. ich küsse dich.")
@@ -7,3 +9,56 @@ weiterbildungsdauer = 4 +2
 print("Hallo liebes Team von " + firmenname + " ich möchte gerne bei euch dem Firma " + firmenname + " arbeiten.ich würde mich über eine Antwort freuen!")
 print("ich habe eine weiterbildung zum " + berufsfeld + " gemacht")
 print(weiterbildungsdauer)
+x = "hallo "
+print(x)
+x= 50.2
+print(x)
+x= 50,2
+print(x)
+x= "Nur cekic"
+print(x[4])
+x="hallo welt"
+print(x[-1])
+x="nur und sude"
+print(x[7:12])
+print(x[0:])
+x= "Dieser Satz ist recht lang.wie lange ist er ?"
+print(len(x))
+x="ich möchte darüber sprechen"
+print(len(x))
+x="hallo"
+x="H" + x[1:]
+print(x)
+x="esra"
+x="E" + x[1:]
+print(x)
+X="hallo"
+print("hallo"+ "10")
+x="nur"
+print("nur"+str(x))
+x="22"
+print(" ich bin " + str(x) +"  jahre alt.")
+x="hallo Welt"
+print(x.upper())
+x="HALLO WELT"
+print(x.lower())
+x="ich lerne Pyhton"
+print(x.upper())
+x="ich ,lerne, Pyhton"
+print(x.split())
+print( 10 + 2 )
+print( 10 - 2 )
+print( 10 * 2 )
+print( 10 / 2 )
+print( 10 // 2 )
+print( 10 % 2 )
+print( 10 ** 2 )
+
+print(type(10))
+print(type(10))
+print(type(5.20))
+print(type(10/2))
+print(type(10%3))
+print(5.2 * 4-2 )
+print(type(5.0 *3))
+print(type(5 *3))
